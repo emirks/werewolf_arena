f6617036e4ed7313b8d722cad43fd3bc489618d5
emirks <emirks88@gmail.com>
2025-07-01 23:42:03 +0300

    Refactor player names list for improved readability and change RUN_SYNTHETIC_VOTES to False

--- werewolf/config.py
+++ werewolf/config.py
@@ -16,13 +16,28 @@ import random
 
 RETRIES = 3
 NAMES = [
-    "Derek", "Scott", "Jacob", "Isaac", "Hayley", "David", "Tyler",
-    "Ginger", "Jackson", "Mason", "Dan", "Bert", "Will", "Sam",
-    "Paul", "Leah", "Harold"
+    "Derek",
+    "Scott",
+    "Jacob",
+    "Isaac",
+    "Hayley",
+    "David",
+    "Tyler",
+    "Ginger",
+    "Jackson",
+    "Mason",
+    "Dan",
+    "Bert",
+    "Will",
+    "Sam",
+    "Paul",
+    "Leah",
+    "Harold",
 ]  # names of famous Werewolves according to Wikipedia
-RUN_SYNTHETIC_VOTES = True
+RUN_SYNTHETIC_VOTES = False
 MAX_DEBATE_TURNS = 8
 NUM_PLAYERS = 8
 
-def get_player_names(): 
+
+def get_player_names():
     return random.sample(NAMES, NUM_PLAYERS)
